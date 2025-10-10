8daef4bcde22928345565f5ac6d49c2228349c43
lepaketjumies <lkamalainen@gmail.com>
2025-10-10 13:26:40 +0300

    initial commit

---
+++ react/omaProjekti/src/App.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1>Lenni Kämäläinen</h1>
+        <nav className="nav">
+          <ul className="li">
+            <a href="">link 1</a>
+          </ul>
+          <ul className="li">
+            <a href="">link 2</a>
+          </ul>
+          <ul className="li">
+            <a href="">link 3</a>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
